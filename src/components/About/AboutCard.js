a84631e0b4a926a358e8968584cdd80f36029d4f
Abdelhadi Bouzani <abdelhadibouzani@gmail.com>
2026-03-21 23:59:33 +0000

    fix about

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,8 +9,8 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
 <p style={{ textAlign: "justify" }}>
   Hello! I’m <span className="purple">Abdelhadi Bouzani</span> from <span className="purple">Morocco</span>.<br />
-  I’m currently pursuing a degree in <span className="purple">Software Engineering</span> at <span className="purple">ENSA</span> and hold a diploma as a <span className="purple">Specialized Technician in Digital Development</span>.<br /><br />
-  I have practical experience through academic projects and a professional internship at <span className="purple">ONEE</span>, where I contributed to real-world software solutions.<br /><br />
+  I’m currently pursuing a degree in <span className="purple">Software Engineering</span> at <span className="purple">ENSIT</span> after completing a <span className="purple">DCA (Diploma of University Studies)</span> for one year at <span className="purple">ENSA Tanger</span>.<br /><br />
+  I hold a diploma as a <span className="purple">Specialized Technician in Digital Development</span> and have gained practical experience through academic projects and a professional internship at <span className="purple">ONEE</span>, where I contributed to real-world software solutions.<br /><br />
   Passionate about technology, I continuously enhance my skills, explore new tools and frameworks, and enjoy staying at the forefront of software development trends. Outside of coding, I like going to the gym and tackling new challenges that help me grow professionally and personally.
 </p>
 
