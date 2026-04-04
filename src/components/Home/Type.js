79706be6b310b2c70239bfd49d96cf5b6f06f28d
Abdelhadi Bouzani <abdelhadibouzani@gmail.com>
2026-04-04 23:54:35 +0100

    Minor updates to About

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -8,9 +8,8 @@ function Type() {
         strings: [
           "Software Developer",
           "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
-        ],
+          "Full Stack Developer",
+                  ],
         autoStart: true,
         loop: true,
         deleteSpeed: 50,
